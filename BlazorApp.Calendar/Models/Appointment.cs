a8f346edfab4369ea32c2b28f709740eb46c811c
Romthes <r.grigorev1502@gmail.com>
2023-12-28 20:48:38 +0300

    готовый проект

--- BlazorApp.Calendar/Models/Appointment.cs
+++ BlazorApp.Calendar/Models/Appointment.cs
@@ -8,10 +8,12 @@ namespace BlazorApp.Calendar.Models
         public string ID { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
+        
         public string Text { get; set; }
         
         // public string Title { get; set; }
         public string Teacher { get; set; }
         
+        public string Client { get; set; }
     }
 }
